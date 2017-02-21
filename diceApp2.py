f76e78678e8704b9933d550052ca99101fef761b
Barterdm <barterdm@gmail.com>
2017-02-21 10:42:54 -0600

    Roll dice based on supplied sides and quantity
    Dice rolling app, you tell it how many sides on your dice and how many
    to roll.  Basic error handlling restarts app.

--- diceApp2.py
+++ diceApp2.py
@@ -1,12 +1,33 @@
 from random import randint
 
-sides = int(input("How man sides?: "))
-dSides = sides
+#Function to roll dice based on input sides and how many to roll
+def dice(many):
+        result = []
+        for i in range(many):
+            result.append(randint(1, dx))
+        print(result)
+        total = print(sum(result))
 
-dSides = randint(1, dSides)
+while True:
+    try:
+        dx = int(input("How many sides for your dice?: "))
+        many = int(input("How many of these do you need to roll?: "))
 
-dNumber = int(input("How many dice do you need to roll?: "))
+        if type(dx) == int:
+            print("Rolling the dice......")
+            dice(many)
+        else:
+            print("OOPSIE")
+    except:
+        print("Only numbers please.")
 
-roll = dSides * dNumber
 
-print(roll)
+
+
+
+
+
+
+
+
+
